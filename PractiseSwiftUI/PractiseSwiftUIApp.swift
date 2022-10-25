4fe9585b80f6da6ab0cac9666ebab1558f275079
Ahmed ibrahim <48734681+ahmed-ibrahim-salim@users.noreply.github.com>
2022-10-25 09:50:07 +0200

    new project created

---
+++ PractiseSwiftUI/PractiseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PractiseSwiftUIApp.swift
+//  PractiseSwiftUI
+//
+//  Created by magdy khalifa on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PractiseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
